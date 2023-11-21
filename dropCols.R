fdb759685712a4cddcd7fca8cec1d6d4595c50e7
W0501759 <W0501759@campus.nscc.ca>
2023-11-21 14:56:46 -0400

    Fixed dataset_columns.csv

--- dropCols.R
+++ dropCols.R
@@ -2,5 +2,5 @@ library(readr)
 library(dplyr)
 data <- read_csv("Data/2019_dataset_en.csv")
 good_columns <- data %>%
-  select(-c(C_YEAR, C_SEV, V_ID, P_ID, P_SAFE))
+  select(-c(C_YEAR, C_SEV, V_ID, P_ID, P_SAFE, C_CASE))
 write_csv(good_columns, "Data/dataset_columns.csv")
