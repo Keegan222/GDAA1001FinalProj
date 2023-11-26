5b782e4ee766d4f6afc3cca08b32c2e330898ba6
Corey Thorne <coreythorne12321@gmail.com>
2023-11-26 12:24:17 -0400

    Script created to convert column types

---
+++ columnClassTyping.R
@@ -0,0 +1,23 @@
+# Convert column types
+
+data2$P_ISEV <- as.factor(data2$P_ISEV)   # 3 target factors, no injury, injury, and fatality
+
+
+data2$C_MNTH <- as.factor(data2$C_MNTH)
+data2$C_WDAY <- as.factor(data2$C_WDAY)
+data2$C_HOUR <- as.factor(data2$C_HOUR)
+# C_VEHS stays numeric
+#C_CONF needs grouping
+data2$C_RCFG <- as.factor(data2$C_RCFG)  # consider grouping
+data2$C_WTHR <- as.factor(data2$C_WTHR) # consider grouping
+data2$C_RSUR <- as.factor(data2$C_RSUR) # consider grouping
+data2$C_RALN <- as.factor(data2$C_RALN) # consider grouping
+data2$C_TRAF <- as.factor(data2$C_TRAF) # consider grouping
+data2$V_TYPE <- as.factor(data2$V_TYPE)
+data2$V_YEAR <- as.factor(data2$V_YEAR)
+data2$P_SEX <- as.factor(data2$P_SEX)
+data2$P_AGE <- as.numeric(data2$P_AGE)
+data2$P_PSN <- as.factor(data2$P_PSN)
+data2$P_USER <- as.factor(data2$P_USER)
+
+
