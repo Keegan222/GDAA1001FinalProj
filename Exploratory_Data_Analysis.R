278c50b6a0ce1890a0ffb0a8ef07f610a8de2545
Walker <W0492401@campus.nscc.ca>
2023-11-28 14:31:07 -0400

    Started on Count data frames for EDA

--- Exploratory_Data_Analysis.R
+++ Exploratory_Data_Analysis.R
@@ -10,10 +10,45 @@ library(ggspatial)
 library(tmap)
 library(grid)
 library(gridExtra)
+library(psych)
 
 #Install and load hexbin for working with large packages
-install.packages('hexbin')
+#install.packages('hexbin')
 library(hexbin)
+
+#Creates a "stats" data frame that holds meta statistics about the data (for example, count, mean, etc)
+stats <- data.frame
+stats <- as.data.frame(summary(data2$Weather), col.names = "count")
+
+#Counts of each category of injury severity for each variable
+Count_PersonSex <- count(data2, vars = c('PersonSex','InjurySeverity'))
+
+Count_Weather <- count(data2, vars = c('Weather','InjurySeverity'))
+
+Count_Month <- count(data2, vars = c('Month','InjurySeverity'))
+
+Count_VehicleYear <- count(data2, vars = c('VehicleYear','InjurySeverity'))
+
+
+
+#Some experimental GGplots (will clean up/fix later)
+ggplot(data2, aes(x=Hour,fill= cut(Hour,24)))+
+  geom_histogram(bins=24)
 
 ggplot(data2)+
   geom_bar(aes(x=Hour, fill = InjurySeverity), position = "dodge")
+
+ggplot(data2)+
+  geom_bar(aes(x=Weather, fill = InjurySeverity), position = "dodge")
+
+ggplot(data2)+
+  geom_bar(aes(x=RoadConfig, fill = InjurySeverity), position = "dodge")
+
+ggplot(data2)+
+  geom_bar(aes(x=RoadSurface, fill = InjurySeverity), position = "dodge")
+
+ggplot(data2, aes(x=PersonSex, color=PersonSex)) +
+  geom_histogram(fill="white", position="dodge")+
+  theme(legend.position="top")
+
+
